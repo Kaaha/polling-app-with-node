99efc035c2dfff0f898a0bfcb4b290ea7969acb2
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-01 14:36:41 +0800

    add view

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 var app=express();
-var http=require('http').Server(app);
-var db=require('./models/db');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+// Adding socket.io
+var io = require('socket.io')(http);
+var db = require('./models/db');
+var feed;
+// On connection to the socket, just invoking the function.
+io.on('connection',function(socket) {
+  feed = require('./models/feeds')(socket);
+});
 
-var dbModel=new db();
-
+/**
+  Adding the controllers.
+*/
+var dbModel = new db();
 dbModel.setupDb();
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/view'));
 app.use(require('./controllers'));
 
-http.listen(3000,function() {
-	console.log('listening on port 3000');
+http.listen(3000, function(){
+  console.log('listening on port 3000');
 });
--- controllers/polls.js
+++ controllers/polls.js
@@ -1,34 +1,43 @@
-var express=require('express');
-var router=express.Router();
-var pollModel=require('../models/polls');
+var express = require('express');
+var router = express.Router();
+// require model file.
+var pollModel = require('../models/polls');
 
 router.route('/')
-	.get(function(req,res) {
-		var pollObject=new pollModel();
-		pollObject.getAllPolls(function(err,pollResponse) {
-			if(err) {
-				return res.json({"responseCode":1,"responseDesc":pollResponse})
-			}
-			res.json({"responseCode":0,"responseDesc":"success","data":pollResponse});
-		});
-	})
-	.post(function(req,res) {
-		var pollObject=new pollModel();
-		pollObject.addNewPolls(req.body,function(err,pollResponse) {
-			if(err) {
-				return res.json({"responseCode":1,"responseDesc":pollResponse});
-			}
-			res.json({"responseCode":0,"responseDesc":"success","data":pollResponse});
-		});
-	})
-	.put(function(req,res) {
-		var pollObject=new pollModel();
-		pollObject.votePollOption(req.body,function(err,pollResponse) {
-			if(err) {
-				return res.json({"responseCode":1,"responseDesc":pollResponse});
-			}
-			return res.json({"responseCode":0,"responseDesc":"success","data":pollResponse});
-		});
-	});
+  .get(function(req,res) {
+    // Code to fetch the polls.
+    var pollObject = new pollModel();
+    // Calling our model function.
+    pollObject.getAllPolls(function(err,pollResponse) {
+      if(err) {
+        return res.json({"responseCode" : 1, "responseDesc" : pollResponse});
+      }
+      res.json({"responseCode" : 0, "responseDesc" : "Success", "data" : pollResponse});
+    });
+  })
+  .post(function(req,res) {
+    // Code to add new polls.
+    var pollObject = new pollModel();
+    // Calling our model function.
+    // We nee to validate our payload here.
+    pollObject.addNewPolls(req.body,function(err,pollResponse) {
+      if(err) {
+        return res.json({"responseCode" : 1, "responseDesc" : pollResponse});
+      }
+      res.json({"responseCode" : 0, "responseDesc" : "Success","data" : pollResponse});
+    });
+  })
+  .put(function(req,res) {
+    // Code to update votes of poll.
+    var pollObject = new pollModel();
+    // Calling our model function.
+    // We need to validate our payload here.
+    pollObject.votePollOption(req.body,function(err,pollResponse) {
+      if(err) {
+        return res.json({"responseCode" : 1, "responseDesc" : pollResponse});
+      }
+      res.json({"responseCode" : 0, "responseDesc" : "Success", "data" : pollResponse});
+    });
+  });
 
-module.exports=router;
+module.exports = router;
